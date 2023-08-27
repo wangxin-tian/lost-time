4abeb711d45e28ae0255ebd421156f03d1468914
wangxin-tian <3106532244@qq.com>
2023-08-27 21:16:59 +0800

    移动开发基础

---
+++ 14.移动开发/1.基本概念.js
@@ -0,0 +1,26 @@
+/*
+
+  视口：
+    viewport:
+    布局视口 == 视觉视口
+    <meta name="viewport" 
+      content="initial-scale=1,width=device-width" />
+    
+    initial-scale=1 初始缩放比是1
+    user-scalable=yes 允许缩放
+    user-scalable=no
+    maximun-scale=5
+    minimum-scale=0.5
+
+    禁止缩放
+    user-scalable=no
+    maximun-scale=1
+    minimum-scale=1
+ */
+
+/*
+  JS获取视口宽度
+  document.documentElement.clientWidth
+  document.documentElement.getBoudingClientRect().width
+*/
+

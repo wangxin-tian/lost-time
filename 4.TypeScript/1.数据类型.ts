9f70e5b152e2ef0d0a60fe6174c3192e594d4c5d
wangxin-tian <3106532244@qq.com>
2023-08-31 20:49:48 +0800

    ts

---
+++ 4.TypeScript/1.数据类型.ts
@@ -0,0 +1,40 @@
+/* 
+  
+
+  TS 类型推演 开发编译报错 支持js最新特性
+
+  配置开发环境
+    https://www.tsdev.cn/tutorials/typescript-in-5-minutes.html
+    node typescript
+
+  tsc name.ts
+  node name.js
+
+  管理 ts
+    dev:
+      lite-server 开发环境的轻量级服务器      lite-server命令
+        根据根目录index文件部署
+
+  工作流
+  ts -> js 
+
+  数据类型
+    基本类型
+      number
+        +num +使得字符串转化成数字
+      string
+      array
+      enum
+      tuple
+      boolean
+      null
+      undefined
+      object
+      void
+      never
+      any
+    高级类型
+      union 组合类型
+      Nullable 可控类型
+      Literal 预定义类型
+*/
